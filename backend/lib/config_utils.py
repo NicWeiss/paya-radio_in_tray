1009e3d8b5a357424a518c01f909f6b48d55a7a0
NWss <anikitin.mm@gmail.com>
2022-03-25 10:52:13 +0300

    fix config loading

--- backend/lib/config_utils.py
+++ backend/lib/config_utils.py
@@ -7,5 +7,5 @@ class ConfigUtils():
 
     def get_config(self, section=None):
         with open(os.path.dirname(__file__) + '/../../config/config.yaml', 'r') as ymlfile:
-            config = yaml.load(ymlfile)
+            config = yaml.safe_load(ymlfile)
         return config[section] if section else config
