1b5498d56005b263376fb6057a2016d1ac5bb1ea
NicWeiss <ixornic@gmail.com>
2022-04-09 22:13:05 +0300

    update install

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo "[Installser] Install Reqirments"
 pip install -r requirements.txt
 
 echo "[Installser] Create link in main menu"
-link="/home/$USER/.local/share/applications/Ya.Radio by NWeiss.desktop"
+link="/home/$USER/.local/share/applications/Ya.Radio_by_NWeiss.desktop"
+path=$(pwd)
 touch "$link"
 cat <<EOF > "$link"
 [Desktop Entry]
@@ -14,8 +15,8 @@ Name=Ya.Radio by NWeiss
 Comment=Ya.Radio by NWeiss
 Exec=./start.sh
 Terminal=false
-Path=/home/$USER/scripts/python/yaradio/
-Icon=/home/$USER/scripts/python/yaradio/backend/assets/icon.png
+Path=$path
+Icon=$path/backend/assets/icon.png
 EOF
 
 
